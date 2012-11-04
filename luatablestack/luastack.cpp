1375437c9f649cd6b590977aff50f73898980442
d-led <dmlled@yahoo.com>
2012-11-04 17:25:36 +0100

    GetTable preparation

--- luatablestack/luastack.cpp
+++ luatablestack/luastack.cpp
@@ -8,44 +8,6 @@ void LuaStack::Append(LuaMultiValue const& V)
 	Values.push_back(V);
 }
 
-static void iterate_and_print(lua_State *L, int index)
-{
-	// Push another reference to the table on top of the stack (so we know
-	// where it is, and this function can work for negative, positive and
-	// pseudo indices
-	lua_pushvalue(L, index);
-	// stack now contains: -1 => table
-	lua_pushnil(L);
-	// stack now contains: -1 => nil; -2 => table
-	printf("{");
-	bool first=true;
-	while (lua_next(L, -2))
-	{
-		// stack now contains: -1 => value; -2 => key; -3 => table
-		// copy the key so that lua_tostring does not modify the original
-		lua_pushvalue(L, -2);
-		// stack now contains: -1 => key; -2 => value; -3 => key; -4 => table
-		std::string key;
-		if (lua_istable(L,-1)) iterate_and_print(L,-1);
-		else  key= lua_tostring(L, -1);
-		std::string value;
-		if (lua_istable(L,-2)) iterate_and_print(L,-2);
-		else value = lua_tostring(L, -2);
-		if (!first) printf(",");
-		if (first) first=false;
-		printf("%s => %s", key.c_str(), value.c_str());
-		// pop value + copy of key, leaving original key
-		lua_pop(L, 2);
-		// stack now contains: -1 => key; -2 => table
-	}
-	// stack now contains: -1 => table (when lua_next returns 0 it pops the key
-	// but does not push anything.)
-	// Pop table
-	lua_pop(L, 1);
-	// Stack is now the same as it was on entry to this function
-	printf("}");
-}
-
 void CrawlStack(lua_State* L,LuaStack& S)
 {
 	int top = lua_gettop(L);
--- luatablestack/luatablecrawler.cpp
+++ luatablestack/luatablecrawler.cpp
@@ -1,8 +1,49 @@
 #include "luatablecrawler.h"
 #include "luavalue.h"
 
+#include <lua.hpp>
+
 TableCrawler::TableCrawler(lua_State* L,int p):L(L),pos(p)
 {
+}
+
+static void GetTable(lua_State *L, int index,std::shared_ptr<LuaTable> T)
+{
+	// Push another reference to the table on top of the stack (so we know
+	// where it is, and this function can work for negative, positive and
+	// pseudo indices
+	lua_pushvalue(L, index);
+	// stack now contains: -1 => table
+	lua_pushnil(L);
+	// stack now contains: -1 => nil; -2 => table
+	while (lua_next(L, -2))
+	{
+		// stack now contains: -1 => value; -2 => key; -3 => table
+		// copy the key so that lua_tostring does not modify the original
+		lua_pushvalue(L, -2);
+		// stack now contains: -1 => key; -2 => value; -3 => key; -4 => table
+		std::string key;
+		if (lua_istable(L,-1)) {
+			std::shared_ptr<LuaTable> tablekey(new LuaTable);
+			GetTable(L,-1,tablekey);
+		}
+		else  key= lua_tostring(L, -1);
+		std::string value;
+		if (lua_istable(L,-2)) {
+			std::shared_ptr<LuaTable> tablevalue(new LuaTable);
+			GetTable(L,-2,tablevalue);
+		}
+		else value = lua_tostring(L, -2);
+		printf("%s => %s", key.c_str(), value.c_str());
+		// pop value + copy of key, leaving original key
+		lua_pop(L, 2);
+		// stack now contains: -1 => key; -2 => table
+	}
+	// stack now contains: -1 => table (when lua_next returns 0 it pops the key
+	// but does not push anything.)
+	// Pop table
+	lua_pop(L, 1);
+	// Stack is now the same as it was on entry to this function
 }
 
 void TableCrawler::Crawl()
