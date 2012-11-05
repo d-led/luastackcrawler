a519d37e9fc4662e9ce404c4c3495c01b8dafa3f
Dmitry Ledentsov <dmlled@yahoo.com>
2012-11-05 15:12:17 +0100

    Update luatablestack/luatablecrawler.h

--- luatablestack/luatablecrawler.h
+++ luatablestack/luatablecrawler.h
@@ -2,7 +2,7 @@
 
 #include "luavalue.h"
 
-#include <memory>
+#include <boost/shared_ptr.hpp>
 #include <map>
 
 struct lua_State;
@@ -11,14 +11,14 @@ class TableCrawler {
 public:
 	TableCrawler(lua_State* L,int p);
 public:
-	std::shared_ptr<LuaTable> GetTable(); //calls Crawl first
+	boost::shared_ptr<LuaTable> GetTable(); //calls Crawl first
 private:
 	void Crawl();
-	void GetTable(lua_State *L, int index,std::shared_ptr<LuaTable> T);
+	void GetTable(lua_State *L, int index,boost::shared_ptr<LuaTable> T);
 private:
-	typedef std::map<const void*,std::shared_ptr<LuaTable> > TableReferences;
+	typedef std::map<const void*,boost::shared_ptr<LuaTable> > TableReferences;
 	lua_State* L;
 	int pos;
-	std::shared_ptr<LuaTable> T;
+	boost::shared_ptr<LuaTable> T;
 	TableReferences tables;
 };
