d08a82559e4008c88e9b24376c402bfbbf6c2020
d-led <dmlled@yahoo.com>
2013-03-18 21:19:44 +0100

    added assertions that don't work

--- simpletest/test.lua
+++ simpletest/test.lua
@@ -1,27 +1,36 @@
-trystack(1)
-trystack(nil)
-trystack("1")
-trystack("1","2")
-trystack({1,2})
-trystack(true,false,3)
+assert(trystack(1)=='1 ')
+assert(trystack(nil)=='nil ')
+assert(trystack("1")=='1 ')
+assert(trystack("1","2")=='1 2 ')
+assert(trystack({1,2})=='{1->1 2->2} ')
+assert(trystack(true,false,3)=='true false 3 ')
 
 t={}
 t[1]="1"
 t[2]="2"
-trystack("1",t)
+assert(trystack("1",t)=='1 {1->1 2->2} ')
 
 t["1"]=1
 t["2"]={3,4,5}
-trystack(nil,t)
+assert(trystack(nil,t)=='nil {1->1 2->2 1->1 2->{1->3 2->4 3->5}} ')
 
 a={}
 a[a]="x";
 a[t]=t;
 a[{1,2}]={3}
 
-trystack(a)
+local res=trystack(a)
+local expected=[[{table*->{1->1 2->2 1->1 2->{1->3 2->4 3->5}} table*->x {1->1 2->2}->{1->3}} ]]
+--print(expected)
+assert(res==expected)
 
-trystack2(1,2)
+res=trystack2(1,2)
+expected='1 2 '
+assert(res==expected)
 
-tryextension("begin",a,"end")
-trystackextension("begin",a,"end") --should produce the same result
+res=tryextension("begin",a,"end")
+expected=[[begin {table*->{1->1 2->2 1->1 2->{1->3 2->4 3->5}} table*->x {1->1 2->2}->{1->3}} ]]
+assert(res==expected)
+
+res=trystackextension("begin",a,"end") --should produce the same result
+assert(res==expected)
