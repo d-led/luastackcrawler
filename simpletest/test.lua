7fa0ff9f8e0d549cc366e5724bf9d69e6c1cb6d0
d-led <dmlled@yahoo.com>
2013-06-24 21:38:06 +0200

    added picojson, LuaBridge

--- simpletest/test.lua
+++ simpletest/test.lua
@@ -1,3 +1,5 @@
+--function assert() end
+
 assert(trystack(1)=='1 ')
 assert(trystack(nil)=='nil ')
 assert(trystack("1")=='1 ')
@@ -8,29 +10,38 @@ assert(trystack(true,false,3)=='true false 3 ')
 t={}
 t[1]="1"
 t[2]="2"
-assert(trystack("1",t)=='1 {1->1 2->2} ')
+local expected = '1 {1->1 2->2} '
+assert(trystack("1",t)==expected)
 
 t["1"]=1
 t["2"]={3,4,5}
-assert(trystack(nil,t)=='nil {1->1 2->2 1->1 2->{1->3 2->4 3->5}} ')
+expected = 'nil {1->1 2->2 1->1 2->{1->3 2->4 3->5}} '
+assert(trystack(nil,t) == expected)
 
 a={}
 a[a]="x";
 a[t]=t;
 a[{1,2}]={3}
 
+expected=[[{table*->{1->1 2->2 1->1 2->{1->3 2->4 3->5}} table*->x {1->1 2->2}->{1->3}} ]]
+io.write('got     : ')
 local res=trystack(a)
-local expected=[[{table*->{1->1 2->2 1->1 2->{1->3 2->4 3->5}} table*->x {1->1 2->2}->{1->3}} ]]
---print(expected)
+print('expected: '..expected)
 assert(res==expected)
 
+io.write('got     : ')
 res=trystack2(1,2)
 expected='1 2 '
+print('expected: '..expected)
 assert(res==expected)
 
+io.write('got     : ')
 res=tryextension("begin",a,"end")
 expected=[[begin {table*->{1->1 2->2 1->1 2->{1->3 2->4 3->5}} table*->x {1->1 2->2}->{1->3}} ]]
+print('expected: '..expected)
 assert(res==expected)
 
+io.write('got     : ')
 res=trystackextension("begin",a,"end") --should produce the same result
+print('expected: '..expected)
 assert(res==expected)
